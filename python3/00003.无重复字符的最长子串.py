70f604f45aaa4706da9145ab5056ef0f1c50e764
wzdnzd <hanxi2014@gmail.com>
2019-05-16 14:55:08 +0800

    init

---
+++ python3/00003.无重复字符的最长子串.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+# https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+#
+# algorithms
+# Medium (27.82%)
+# Likes:    1809
+# Dislikes: 0
+# Total Accepted:    122.4K
+# Total Submissions: 415.8K
+# Testcase Example:  '"abcabcbb"'
+#
+# 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+#
+# 示例 1:
+#
+# 输入: "abcabcbb"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+#
+#
+# 示例 2:
+#
+# 输入: "bbbbb"
+# 输出: 1
+# 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+#
+#
+# 示例 3:
+#
+# 输入: "pwwkew"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+# 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+#
+#
+#
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        lookup = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in lookup:
+                i = max(lookup[s[j]], i)
+            ans = max(ans, j - i + 1)
+            lookup[s[j]] = j + 1
+        return ans
