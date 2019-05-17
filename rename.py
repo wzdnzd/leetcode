680f89c2f3899e11032794c6d18e1293fb87c70f
wzdnzd <hanxi2014@gmail.com>
2019-05-17 15:32:32 +0800

    format file name

--- rename.py
+++ rename.py
@@ -5,15 +5,23 @@ PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def rename(n):
+    if n <= 0:
+        return
+
     for root, _, files in os.walk(PATH, topdown=True):
         for f in files:
             if re.match(r'\d+\.\w+', f):
                 index = f.find('.')
 
-                if index >= n:
+                if index == n:
                     continue
+                elif n > index:
+                    filename = f[:index].zfill(n) + f[index:]
+                else:
+                    filename = f[:index][-n:] + f[index:]
 
-                filename = os.path.join(root, f[:index].zfill(n) + f[index:])
+                filename = os.path.join(root, filename)
+
                 if os.path.exists(filename):
                     os.remove(filename)
 
@@ -21,4 +29,4 @@ def rename(n):
 
 
 if __name__ == "__main__":
-    rename(5)
+    rename(4)
