7eb1351c5483f5cd9a575ff9ec4ca282844c378e
wzdnzd <hanxi2014@gmail.com>
2019-06-03 15:56:04 +0800

    init

---
+++ mysql/0184.找出共同微信群.最多的用户.sql
@@ -0,0 +1,31 @@
+--
+-- @lc app=leetcode.cn id=184 lang=mysql
+--
+-- [184] 共同微信群最多的用户
+--
+-- database
+--
+-- 已知 用户-微信群 关系表(1亿用户量)，给定一个用户id=1234，使用SQL找出与该用户共同微信群最多的top3个用户
+-- 
+-- t 表：
+-- 
+-- +-----------+-----------+
+-- | user_id  | wxgroup_id |
+-- +-----------+-----------+
+-- | 1234    |  38809303   |
+-- | 2645    |  58675756   |
+-- | 3868    |  85686935   |
+-- | 1234    |  58675756   |
+-- +-----------+-----------+
+-- 
+-- 
+-- 
+--
+# Write your MySQL query statement below
+SELECT t1.user_id user_id, COUNT(*) cnt
+FROM t t1
+JOIN (SELECT * FROM t WHERE t.user_id=1234) t2 ON t1.wxgroup_id=t2.wxgroup_id AND t1.user_id != t2.user_id
+GROUP BY t1.user_id
+ORDER BY cnt DESC
+LIMIT 3
+
