a2b53146cf4eb498f0ac7a4272a4424a388c632c
wzdnzd <hanxi2014@gmail.com>
2022-09-07 15:26:00 +0800

    do some practice

---
+++ java/0083.删除排序链表中的重复元素.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=java
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (53.42%)
+ * Likes:    849
+ * Dislikes: 0
+ * Total Accepted:    482.6K
+ * Total Submissions: 903.4K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序 排列
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode p1 = head, p2 = head.next;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                p1.next = p2;
+                p1 = p1.next;
+            }
+
+            p2 = p2.next;
+        }
+
+        p1.next = null;
+        return head;
+    }
+}
+// @lc code=end
