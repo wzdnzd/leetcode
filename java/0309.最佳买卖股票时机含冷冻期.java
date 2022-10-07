3cb2f1bb63d3da809b468a732b8474f179c33423
wzdnzd <hanxi2014@gmail.com>
2022-10-07 10:48:55 +0800

    dynamic programing for stock

---
+++ java/0309.最佳买卖股票时机含冷冻期.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=309 lang=java
+ *
+ * [309] 最佳买卖股票时机含冷冻期
+ *
+ * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+ *
+ * algorithms
+ * Medium (63.65%)
+ * Likes:    1329
+ * Dislikes: 0
+ * Total Accepted:    213K
+ * Total Submissions: 334K
+ * Testcase Example:  '[1,2,3,0,2]'
+ *
+ * 给定一个整数数组prices，其中第  prices[i] 表示第 i 天的股票价格 。​
+ * 
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ * 
+ * 
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ * 
+ * 
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: prices = [1,2,3,0,2]
+ * 输出: 3 
+ * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: prices = [1]
+ * 输出: 0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= prices.length <= 5000
+ * 0 <= prices[i] <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sell = 0, buy = Integer.MIN_VALUE, preProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            int value = sell;
+            sell = Math.max(sell, buy + prices[i]);
+            buy = Math.max(buy, preProfit - prices[i]);
+            preProfit = value;
+        }
+
+        return sell;
+    }
+}
+// @lc code=end
