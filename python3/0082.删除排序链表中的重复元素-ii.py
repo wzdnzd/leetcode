3e989e8f5a549dc1fd5fb0e7d70f513a0d32c368
wzdnzd <hanxi2014@gmail.com>
2023-05-19 11:19:32 +0800

    do some exercises

---
+++ python3/0082.删除排序链表中的重复元素-ii.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode.cn id=82 lang=python3
+#
+# [82] 删除排序链表中的重复元素 II
+#
+# https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/description/
+#
+# algorithms
+# Medium (53.57%)
+# Likes:    1005
+# Dislikes: 0
+# Total Accepted:    299.9K
+# Total Submissions: 560K
+# Testcase Example:  '[1,2,3,3,4,4,5]'
+#
+# 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：head = [1,2,3,3,4,4,5]
+# 输出：[1,2,5]
+#
+#
+# 示例 2：
+#
+#
+# 输入：head = [1,1,1,2,3]
+# 输出：[2,3]
+#
+#
+#
+#
+# 提示：
+#
+#
+# 链表中节点数目在范围 [0, 300] 内
+# -100 <= Node.val <= 100
+# 题目数据保证链表已经按升序 排列
+#
+#
+#
+
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        dummy = ListNode(next=head)
+        pre = dummy
+        while head and head.next:
+            if head.val == head.next.val:
+                num = head.val
+                while head and head.val == num:
+                    head = head.next
+                pre.next = head
+            else:
+                pre = pre.next
+                head = head.next
+
+        return dummy.next
+
+
+# @lc code=end
