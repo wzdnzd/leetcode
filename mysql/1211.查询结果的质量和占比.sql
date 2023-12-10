0ecce50a868e304962eb3ff0612a6c790b24c337
wzdnzd <hanxi2014@gmail.com>
2023-12-10 14:43:29 +0800

    practice

---
+++ mysql/1211.查询结果的质量和占比.sql
@@ -0,0 +1,98 @@
+--
+-- @lc app=leetcode.cn id=1211 lang=mysql
+--
+-- [1211] 查询结果的质量和占比
+--
+-- https://leetcode.cn/problems/queries-quality-and-percentage/description/
+--
+-- database
+-- Easy (66.56%)
+-- Likes:    78
+-- Dislikes: 0
+-- Total Accepted:    32.4K
+-- Total Submissions: 49.6K
+-- Testcase Example:  '{"headers":{"Queries":["query_name","result","position","rating"]},"rows":{"Queries":[["Dog","Golden Retriever",1,5],["Dog","German Shepherd",2,5],["Dog","Mule",200,1],["Cat","Shirazi",5,2],["Cat","Siamese",3,3],["Cat","Sphynx",7,4]]}}'
+--
+-- Queries 表： 
+-- 
+-- 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | query_name  | varchar |
+-- | result      | varchar |
+-- | position    | int     |
+-- | rating      | int     |
+-- +-------------+---------+
+-- 此表可能有重复的行。
+-- 此表包含了一些从数据库中收集的查询信息。
+-- “位置”（position）列的值为 1 到 500 。
+-- “评分”（rating）列的值为 1 到 5 。评分小于 3 的查询被定义为质量很差的查询。
+-- 
+-- 
+-- 
+-- 
+-- 将查询结果的质量 quality 定义为：
+-- 
+-- 
+-- 各查询结果的评分与其位置之间比率的平均值。
+-- 
+-- 
+-- 将劣质查询百分比 poor_query_percentage 为：
+-- 
+-- 
+-- 评分小于 3 的查询结果占全部查询结果的百分比。
+-- 
+-- 
+-- 编写解决方案，找出每次的 query_name 、 quality 和 poor_query_percentage。
+-- 
+-- quality 和 poor_query_percentage 都应 四舍五入到小数点后两位 。
+-- 
+-- 以 任意顺序 返回结果表。
+-- 
+-- 结果格式如下所示：
+-- 
+-- 
+-- 
+-- 示例 1：
+-- 
+-- 
+-- 输入：
+-- Queries table:
+-- +------------+-------------------+----------+--------+
+-- | query_name | result            | position | rating |
+-- +------------+-------------------+----------+--------+
+-- | Dog        | Golden Retriever  | 1        | 5      |
+-- | Dog        | German Shepherd   | 2        | 5      |
+-- | Dog        | Mule              | 200      | 1      |
+-- | Cat        | Shirazi           | 5        | 2      |
+-- | Cat        | Siamese           | 3        | 3      |
+-- | Cat        | Sphynx            | 7        | 4      |
+-- +------------+-------------------+----------+--------+
+-- 输出：
+-- +------------+---------+-----------------------+
+-- | query_name | quality | poor_query_percentage |
+-- +------------+---------+-----------------------+
+-- | Dog        | 2.50    | 33.33                 |
+-- | Cat        | 0.66    | 33.33                 |
+-- +------------+---------+-----------------------+
+-- 解释：
+-- Dog 查询结果的质量为 ((5 / 1) + (5 / 2) + (1 / 200)) / 3 = 2.50
+-- Dog 查询结果的劣质查询百分比为 (1 / 3) * 100 = 33.33
+-- 
+-- Cat 查询结果的质量为 ((2 / 5) + (3 / 3) + (4 / 7)) / 3 = 0.66
+-- Cat 查询结果的劣质查询百分比为 (1 / 3) * 100 = 33.33
+-- 
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT 
+    query_name, 
+    ROUND(AVG(rating/position), 2) quality,
+    ROUND(SUM(IF(rating < 3, 1, 0)) * 100 / COUNT(*), 2) poor_query_percentage
+FROM Queries
+GROUP BY query_name HAVING query_name IS NOT NULL;
+-- @lc code=end
+
