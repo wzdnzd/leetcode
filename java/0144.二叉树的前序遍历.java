4718b9aabff810941aed0594c301780d186d9e8b
wzdnzd <hanxi2014@gmail.com>
2024-02-12 08:19:22 +0800

    practice

---
+++ java/0144.二叉树的前序遍历.java
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=java
+ *
+ * [144] 二叉树的前序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (71.50%)
+ * Likes:    1228
+ * Dislikes: 0
+ * Total Accepted:    1M
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderTraversalHelper(root, result);
+
+        return result;
+    }
+
+    private void preorderTraversalHelper(TreeNode root, List<Integer> result) {
+        if (root == null)
+            return;
+
+        result.add(root.val);
+        preorderTraversalHelper(root.left, result);
+        preorderTraversalHelper(root.right, result);
+    }
+}
+// @lc code=end
