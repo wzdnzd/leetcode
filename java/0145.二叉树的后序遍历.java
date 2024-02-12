64afdadaadd1f880c2f07b38ddbca85a8a39fcd9
wzdnzd <hanxi2014@gmail.com>
2024-02-12 08:19:22 +0800

    practice

---
+++ java/0145.二叉树的后序遍历.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=java
+ *
+ * [145] 二叉树的后序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (76.40%)
+ * Likes:    1146
+ * Dislikes: 0
+ * Total Accepted:    723.3K
+ * Total Submissions: 946.4K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[3,2,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点的数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorderTraversalHelper(root, result);
+
+        return result;
+    }
+
+    private void postorderTraversalHelper(TreeNode root, List<Integer> result) {
+        if (root == null)
+            return;
+
+        postorderTraversalHelper(root.left, result);
+        postorderTraversalHelper(root.right, result);
+        result.add(root.val);
+    }
+}
+// @lc code=end
