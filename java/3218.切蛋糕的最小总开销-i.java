549da307111bbb451b4b94deeb3de2776fb4cb61
wzdnzd <hanxi2014@gmail.com>
2024-12-25 07:58:34 +0800

    practice

---
+++ java/3218.切蛋糕的最小总开销-i.java
@@ -0,0 +1,133 @@
+/*
+ * @lc app=leetcode.cn id=3218 lang=java
+ *
+ * [3218] 切蛋糕的最小总开销 I
+ *
+ * https://leetcode.cn/problems/minimum-cost-for-cutting-cake-i/description/
+ *
+ * algorithms
+ * Medium (54.26%)
+ * Likes:    13
+ * Dislikes: 0
+ * Total Accepted:    5K
+ * Total Submissions: 8.8K
+ * Testcase Example:  '3\n2\n[1,3]\n[5]'
+ *
+ * 有一个 m x n 大小的矩形蛋糕，需要切成 1 x 1 的小块。
+ * 
+ * 给你整数 m ，n 和两个数组：
+ * 
+ * 
+ * horizontalCut 的大小为 m - 1 ，其中 horizontalCut[i] 表示沿着水平线 i 切蛋糕的开销。
+ * verticalCut 的大小为 n - 1 ，其中 verticalCut[j] 表示沿着垂直线 j 切蛋糕的开销。
+ * 
+ * 
+ * 一次操作中，你可以选择任意不是 1 x 1 大小的矩形蛋糕并执行以下操作之一：
+ * 
+ * 
+ * 沿着水平线 i 切开蛋糕，开销为 horizontalCut[i] 。
+ * 沿着垂直线 j 切开蛋糕，开销为 verticalCut[j] 。
+ * 
+ * 
+ * 每次操作后，这块蛋糕都被切成两个独立的小蛋糕。
+ * 
+ * 每次操作的开销都为最开始对应切割线的开销，并且不会改变。
+ * 
+ * 请你返回将蛋糕全部切成 1 x 1 的蛋糕块的 最小 总开销。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：m = 3, n = 2, horizontalCut = [1,3], verticalCut = [5]
+ * 
+ * 输出：13
+ * 
+ * 解释：
+ * 
+ * 
+ * 
+ * 
+ * 沿着垂直线 0 切开蛋糕，开销为 5 。
+ * 沿着水平线 0 切开 3 x 1 的蛋糕块，开销为 1 。
+ * 沿着水平线 0 切开 3 x 1 的蛋糕块，开销为 1 。
+ * 沿着水平线 1 切开 2 x 1 的蛋糕块，开销为 3 。
+ * 沿着水平线 1 切开 2 x 1 的蛋糕块，开销为 3 。
+ * 
+ * 
+ * 总开销为 5 + 1 + 1 + 3 + 3 = 13 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：m = 2, n = 2, horizontalCut = [7], verticalCut = [4]
+ * 
+ * 输出：15
+ * 
+ * 解释：
+ * 
+ * 
+ * 沿着水平线 0 切开蛋糕，开销为 7 。
+ * 沿着垂直线 0 切开 1 x 2 的蛋糕块，开销为 4 。
+ * 沿着垂直线 0 切开 1 x 2 的蛋糕块，开销为 4 。
+ * 
+ * 
+ * 总开销为 7 + 4 + 4 = 15 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= m, n <= 20
+ * horizontalCut.length == m - 1
+ * verticalCut.length == n - 1
+ * 1 <= horizontalCut[i], verticalCut[i] <= 10^3
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+
+class Solution {
+    public int minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
+        Arrays.sort(horizontalCut);
+        Arrays.sort(verticalCut);
+
+        int minCost = 0;
+        int horizontalIndex = m - 2, verticalIndex = n - 2;
+        int horizontalCount = 1, verticalCount = 1;
+
+        while (horizontalIndex >= 0 && verticalIndex >= 0) {
+            if (horizontalCut[horizontalIndex] >= verticalCut[verticalIndex]) {
+                minCost += horizontalCut[horizontalIndex] * horizontalCount;
+                horizontalIndex--;
+                verticalCount++;
+            } else {
+                minCost += verticalCut[verticalIndex] * verticalCount;
+                verticalIndex--;
+                horizontalCount++;
+            }
+        }
+
+        while (horizontalIndex >= 0) {
+            minCost += horizontalCut[horizontalIndex] * horizontalCount;
+            horizontalIndex--;
+            verticalCount++;
+        }
+
+        while (verticalIndex >= 0) {
+            minCost += verticalCut[verticalIndex] * verticalCount;
+            verticalIndex--;
+            horizontalCount++;
+        }
+
+        return minCost;
+    }
+}
+// @lc code=end
