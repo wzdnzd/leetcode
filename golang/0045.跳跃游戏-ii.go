054a348c8711aebb952e4113680faf7104f717f1
wzdnzd <hanxi2014@gmail.com>
2025-01-27 00:03:59 +0800

    practice

---
+++ golang/0045.跳跃游戏-ii.go
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=golang
+ *
+ * [45] 跳跃游戏 II
+ *
+ * https://leetcode.cn/problems/jump-game-ii/description/
+ *
+ * algorithms
+ * Medium (44.50%)
+ * Likes:    2714
+ * Dislikes: 0
+ * Total Accepted:    870.6K
+ * Total Submissions: 2M
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+ *
+ * 每个元素 nums[i] 表示从索引 i 向后跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j]
+ * 处:
+ *
+ *
+ * 0 <= j <= nums[i]
+ * i + j < n
+ *
+ *
+ * 返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: nums = [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ * 从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: nums = [2,3,0,1,4]
+ * 输出: 2
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * 0 <= nums[i] <= 1000
+ * 题目保证可以到达 nums[n-1]
+ *
+ *
+ */
+
+// @lc code=start
+package main
+
+func jump(nums []int) int {
+	currentRight, nextRight, count := 0, 0, 0
+
+	for i, num := range nums[:len(nums)-1] {
+		nextRight = max(nextRight, i+num)
+		if i == currentRight {
+			currentRight = nextRight
+			count++
+		}
+	}
+
+	return count
+}
+
+// @lc code=end
