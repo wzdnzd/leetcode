51b0812806a6efcd6344388ce7582d058cef366a
wzdnzd <hanxi2014@gmail.com>
2025-04-17 07:43:29 +0800

    practice

---
+++ golang/2176.统计数组中相等且可以被整除的数对.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=2176 lang=golang
+ *
+ * [2176] 统计数组中相等且可以被整除的数对
+ *
+ * https://leetcode.cn/problems/count-equal-and-divisible-pairs-in-an-array/description/
+ *
+ * algorithms
+ * Easy (78.70%)
+ * Likes:    25
+ * Dislikes: 0
+ * Total Accepted:    16.1K
+ * Total Submissions: 20.3K
+ * Testcase Example:  '[3,1,2,2,2,1,3]\n2'
+ *
+ * 给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k ，请你返回满足 0 <= i < j < n ，nums[i] ==
+ * nums[j] 且 (i * j) 能被 k 整除的数对 (i, j) 的 数目 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [3,1,2,2,2,1,3], k = 2
+ * 输出：4
+ * 解释：
+ * 总共有 4 对数符合所有要求：
+ * - nums[0] == nums[6] 且 0 * 6 == 0 ，能被 2 整除。
+ * - nums[2] == nums[3] 且 2 * 3 == 6 ，能被 2 整除。
+ * - nums[2] == nums[4] 且 2 * 4 == 8 ，能被 2 整除。
+ * - nums[3] == nums[4] 且 3 * 4 == 12 ，能被 2 整除。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3,4], k = 1
+ * 输出：0
+ * 解释：由于数组中没有重复数值，所以没有数对 (i,j) 符合所有要求。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i], k <= 100
+ *
+ *
+ */
+
+// @lc code=start
+package main
+
+func countPairs(nums []int, k int) int {
+	n, count := len(nums), 0
+
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n; j++ {
+			if nums[i] == nums[j] && (i*j)%k == 0 {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+// @lc code=end
