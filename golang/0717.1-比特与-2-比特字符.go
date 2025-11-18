937776a23d8e1c6ae95adcd583ac44449319731f
wzdnzd <hanxi2014@gmail.com>
2025-11-18 08:00:23 +0800

    practice

---
+++ golang/0717.1-比特与-2-比特字符.go
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=717 lang=golang
+ *
+ * [717] 1 比特与 2 比特字符
+ *
+ * https://leetcode.cn/problems/1-bit-and-2-bit-characters/description/
+ *
+ * algorithms
+ * Easy (55.52%)
+ * Likes:    317
+ * Dislikes: 0
+ * Total Accepted:    78.5K
+ * Total Submissions: 140.8K
+ * Testcase Example:  '[1,0,0]'
+ *
+ * 有两种特殊字符：
+ *
+ *
+ * 第一种字符可以用一比特 0 表示
+ * 第二种字符可以用两比特（10 或 11）表示
+ *
+ *
+ * 给你一个以 0 结尾的二进制数组 bits ，如果最后一个字符必须是一个一比特字符，则返回 true 。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: bits = [1, 0, 0]
+ * 输出: true
+ * 解释: 唯一的解码方式是将其解析为一个两比特字符和一个一比特字符。
+ * 所以最后一个字符是一比特字符。
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入：bits = [1,1,1,0]
+ * 输出：false
+ * 解释：唯一的解码方式是将其解析为两比特字符和两比特字符。
+ * 所以最后一个字符不是一比特字符。
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= bits.length <= 1000
+ * bits[i] 为 0 或 1
+ *
+ *
+ */
+
+// @lc code=start
+package main
+
+func isOneBitCharacter(bits []int) bool {
+	index, n := 0, len(bits)
+	for index < n {
+		if index == n-1 {
+			return true
+		}
+
+		if bits[index] == 1 {
+			index += 2
+		} else {
+			index++
+		}
+	}
+
+	return false
+}
+
+// @lc code=end
