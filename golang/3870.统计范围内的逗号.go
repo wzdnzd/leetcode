dc13a11ea518aa30cc2b41fe262a6b165e3e87cb
wzdnzd <hanxi2014@gmail.com>
2026-09-08 09:11:39 +0800

    practice

---
+++ golang/3870.统计范围内的逗号.go
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=3870 lang=golang
+ *
+ * [3870] 统计范围内的逗号
+ *
+ * https://leetcode.cn/problems/count-commas-in-range/description/
+ *
+ * algorithms
+ * Easy (70.04%)
+ * Likes:    6
+ * Dislikes: 0
+ * Total Accepted:    5.8K
+ * Total Submissions: 7.8K
+ * Testcase Example:  '1002'
+ *
+ * 给你一个整数 n。
+ *
+ * 返回将所有从 [1, n]（包含两端）范围内的整数以 标准 数字格式书写时所用到的 逗号总数。
+ *
+ * 在 标准 格式中：
+ *
+ *
+ * 从右边开始，每 三位 数字后插入一个逗号。
+ * 位数 少于四位 的数字不包含逗号。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入： n = 1002
+ *
+ * 输出： 3
+ *
+ * 解释：
+ *
+ * 数字 "1,000"、"1,001" 和 "1,002" 每个都包含一个逗号，总计 3 个逗号。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入： n = 998
+ *
+ * 输出： 0
+ *
+ * 解释：
+ *
+ * 从 1 到 998 的所有数字位数都少于四位，因此没有使用逗号。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= n <= 10^5
+ *
+ *
+ */
+
+// @lc code=start
+package main
+
+func countCommas(n int) int {
+	count := 0
+
+	for low := 1000; low <= n; low *= 1000 {
+		count += n - low + 1
+	}
+
+	return count
+}
+
+// @lc code=end
